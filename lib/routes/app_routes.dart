159b26a8f2c30ac56c7e1d8c566e9e4e6f883152
INDO SAKURA <indosakura@INDOs-MacBook-Air.local>
2026-03-30 17:19:13 +0530

    fixed

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -2,5 +2,6 @@ class AppRoutes {
   static const splashScreen = '/splash_screen',
       onboardScreen = '/onboard_screen',
       landingScreen = '/landing_screen',
+      verifyEmailScreen = "/verify_email_screen",
       loginScreen = '/login_screen';
 }
